47f48fa39a6835d00421281ff59b5e4632c4f26c
Santiago Trini <santiagotrini@gmail.com>
2022-08-10 17:55:16 -0300

    clase del 10/8/22

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 // imports
 import express from 'express';
+import mongoose from 'mongoose';
 // config vars
 const PORT = process.env.PORT || 3000;
+const DB   = process.env.DB   || 'mongodb://127.0.0.1/notes';
+
+mongoose.connect(DB)
+  .then(() => console.log('DB conectada'))
+  .catch(err => console.log(err));
+
+const NoteSchema = new mongoose.Schema({
+  text: String,
+  done: { type: Boolean, default: false }
+});
+const Note = mongoose.model('Note', NoteSchema);
+
+const notes = [];
 // base de datos de mentira
-const notes = [
-  {
-    id: 1,
-    text: 'Lavar la ropa',
-    done: false
-  },
-  {
-    id: 2,
-    text: 'Estudiar node',
-    done: false
-  },
-  {
-    id: 3,
-    text: 'Hacer las compras',
-    done: false
-  },
-  {
-    id: 4,
-    text: 'Preparar los examenes',
-    done: false
-  }
-];
+// const notes = [
+//   {
+//     id: 1,
+//     text: 'Lavar la ropa',
+//     done: false
+//   },
+//   {
+//     id: 2,
+//     text: 'Estudiar node',
+//     done: false
+//   },
+//   {
+//     id: 3,
+//     text: 'Hacer las compras',
+//     done: false
+//   },
+//   {
+//     id: 4,
+//     text: 'Preparar los examenes',
+//     done: false
+//   }
+// ];
 
 
 const app = express();
@@ -40,21 +54,28 @@ app.use(express.json());
 
 // devuelve todas las notas
 app.get('/notes', (req, res) => {
-  res.status(200).json(notes);
+  Note.find((err, notes) => {
+    res.status(200).json(notes);
+  });
 });
 // devuelve la nota con id: id
 app.get('/notes/:id', (req, res) => {
-  let id = +req.params.id;
-  let result = notes.filter(note => note.id === id);
-  res.status(200).json(result);
+  let id = req.params.id;
+  Note.findById(id, (err, note) => {
+    res.status(200).json(note);
+  });
+  // let result = notes.filter(note => note.id === id);
 });
 // crea una nota nueva
 app.post('/notes', (req, res) => {
-  let { id, text, done } = req.body;
+  let { text } = req.body;
   console.log(req.body);
-  let newNote = { id, text, done };
-  notes.push(newNote);
-  res.status(201).json(newNote);
+  let newNote = new Note({ text });
+  // notes.push(newNote);
+  newNote.save((err, note) => {
+    // if (err) throw err;
+    res.status(201).json(note);
+  });
 });
 
 app.listen(PORT, () => {
